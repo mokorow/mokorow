1ec0a909bb17c403de9f6465ce3361d8973fd5c6
mokorow <57408566+mokorow@users.noreply.github.com>
2019-11-24 13:56:52 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <ul class="inner2">
+        <li class="footer-photo backcolor"><img src="<?php echo get_template_directory_uri(); ?>/img/top-img/footer-bg.png" alt="" class=""></li>
+        <li class="logo pad10"><img src="<?php echo get_template_directory_uri(); ?>/img/top-img/header-logo.png" alt="しま農園"　class="" width="120"></li>
+        <li>
+            <div class="text3 pad10">〒123-4567 〇〇県〇〇郡〇〇村831</div>
+        </li>
+        <li>
+            <div class="text3 pad10">TEL:098-7654-3210  FAX:123-4567-831</div>
+        </li>
+        <li>
+            <div class="inner3 inner4 pad30">
+                <p class="text1">配送・お支払いについて</p>
+                <p class="text1 separate">プライバシーポリシー</p>
+                <p class="text1">お問い合わせ</p>
+            </div>
+        </li>
+        <li class="footer-end">
+            <div class="text4">Copyright ©︎ iSara All Rights Reserved.</div>
+        </li>
+    </ul>
+</footer>
---
+++ header.php
@@ -0,0 +1,27 @@
+<header>
+        <div class="box">
+            <ul class="inner">
+                <li class="about-link head-box">
+                    <div class="text1">当園について</div>
+                    <div class="text2">ABOUT</div>
+                </li>
+                <li class="news-link head-box">
+                    <div class="text1">お知らせ</div>
+                    <div class="text2">NEWS</div>
+                </li>
+                <li><img src="<?php echo get_template_directory_uri(); ?>/img/top-img/header-logo.png" alt="しま農園"　class="logo" height="90" width="80"></li>
+                <li class="store-link head-box">
+                    <div class="text1">品目・ストア</div>
+                    <div class="text2">STORE</div>
+                </li>
+                <li class="contant-link head-box">
+                    <div class="text1">お問い合わせ</div>
+                    <div class="text2">CONTACT</div>
+                </li>
+            </ul>
+            <ul class="inner-sp">
+                <li class="logo2"><img src="<?php echo get_template_directory_uri(); ?>/img/top-img/header-logo.png" alt="しま農園"　class="" width="50"></li>
+                <li class="logo3"><img src="<?php echo get_template_directory_uri(); ?>/img/hamburger-icon.png" alt=""　width="38.5"></li>
+            </ul>
+        </div>     
+</header>
